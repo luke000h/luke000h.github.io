f00f7ab01d61ad29702ff21d422957fea829b528
luke000h <66697311+luke000h@users.noreply.github.com>
2024-07-29 11:31:03 +0100

    Create Site.swift

---
+++ Sources/Site.swift
@@ -0,0 +1,28 @@
+import Foundation
+import Ignite
+
+@main
+struct IgniteWebsite {
+    static func main() async {
+        let site = ExampleSite()
+
+        do {
+            try await site.publish()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct ExampleSite: Site {    
+    var name = "Hello World"
+    var titleSuffix = " – My Awesome Site"
+    var url = URL("https://luke000h.github.io")
+    var builtInIconsEnabled = true
+
+    var author = "John Appleseed"
+
+    var homePage = Home()
+    var theme = MyTheme()
+}
+
